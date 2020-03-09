16e78b3d465ee42d6bfaa9fd12befa4c85cd07db
Dodoma95 <maillard.dorian60@gmail.com>
2020-03-09 15:18:27 +0100

    add card with markers for all stations in lyon

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,5 @@ class HomeController < ApplicationController
   def index
     @stations = Station.all
   end
-  #7623efb5eb3326b5432f0e594a1ba6815aab1b5c
+  #clé API JCDECAUX velib 7623efb5eb3326b5432f0e594a1ba6815aab1b5c
 end
